d65fc078682079908537e6316a6dc46065ad3d4f
Luke Mullikin <lukemull11@gmail.com>
2021-03-26 17:58:29 -0400

    add EEA

---
+++ eea.py
@@ -0,0 +1,41 @@
+# Extended Euclidean Algorithm
+# Purpose: compute gcd and Bezout Identity
+
+# receive operands from user
+print("Enter 2 numbers to find gcd(a, b)")
+valA = int(input("a = "))
+valB = int(input("b = "))
+
+# put greatest value in b if necessary
+if valA >= valB:
+    temp = valB
+    valB = valA
+    valA = temp
+
+# save initial values 
+startA = valA
+startB = valB
+
+# perform Euclidean Algorithm to compute greatest common divisor
+floor_list = [0]
+while valA != 0:
+    floorba = valB // valA
+    floor_list.append(floorba)
+    newA = valB - floorba * valA
+    valB = valA
+    valA = newA
+
+# print gcd
+print("\ngcd(", startA, ",", startB, ") =", valB, "\n")
+
+# construct Bezout Identity
+x = 1
+y = 0
+while len(floor_list) > 1:
+    yn = x - (floor_list.pop() * y)
+    x = y
+    y = yn
+
+# print Bezout Identity
+print("Bezout Identity:")
+print(valB, "=", startB, "(", x, ") +", startA, "(", y, ")")
